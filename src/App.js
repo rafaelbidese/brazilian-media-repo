320c69b3214b9342835da57072cc9ba4714a0166
Rafael Bidese <rafaelbidese@gmail.com>
2020-05-21 01:47:01 -0500

    Apply materialui to the interface

--- src/App.js
+++ src/App.js
@@ -2,8 +2,36 @@ import React from 'react';
 import { ContentfulClient, ContentfulProvider, useContentful} from 'react-contentful';
 import './App.css';
 import apiconfig from './config.js'
+import { Card, CardContent, Typography, Avatar, Link, Chip, CardActions} from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles';
 
-const Page = props => {
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    overflow: 'hidden',
+    padding: theme.spacing(0, 3),
+  },
+  paper: {
+    maxWidth: 600,
+    margin: `${theme.spacing(1)}px auto`,
+    padding: theme.spacing(2),
+  },
+  media : {
+    display: "flex",
+    margin: "10px 50px",
+  },
+  image: {
+    height:"300px",
+    width: "200px",
+    margin: "auto",
+  }
+}));
+ 
+ const Page = props => {
+
+  const classes = useStyles();
+
   const { data, error, fetched, loading } = useContentful({
     contentType: 'info',
     query: {
@@ -34,40 +62,29 @@ const Page = props => {
     <div style={{display: "flex", flexDirection:"column", justifyContent: "center"}}>
       <h2 style={{textAlign:"center"}}>Brazilian media repository</h2>
       {data.items.map(item => (
-        <article 
-          style={{
-            width: "90vw",
-            backgroundColor: `${item.fields.tags["tags"][0] === "series" ? "#f7f7f7" : "#f7e7f7"}`,
-            margin: "20px",
-            padding: "10px",
-            display: "flex",
-            flexDirection: "row",
-            alignItems: "center",
-            borderRadius: "5px",
-            boxShadow: "0 0 5px 0px"}} >
-          <img 
-            src={item.fields.thumbnail.fields.file.url} 
-            style={{
-              width:"200px",
-              height: "200px",
-              margin: "20px"}}
-            alt="Movie Thumbnail"></img>
-          <div>
-            <h1>{item.fields.titleEn}  ({item.fields.year})</h1>
-            <h3>{item.fields.titleBr}</h3>
-            <ul>
-              <li>{item.fields.tags["tags"][0]}</li>
-            </ul>
-            <p>{item.fields.description}</p>
-            <p>{item.fields.author}</p>
-            <a href={item.fields.url}>[Trailer]</a>
-          </div>
-        </article>
+        <Card className={classes.media}>
+          <Avatar 
+          variant="square"
+          alt="Remy Sharp"
+          className={classes.image}
+          src={item.fields.thumbnail.fields.file.url}
+          />
+          <CardContent>
+            <Typography gutterBottom variant="subtitle1">{item.fields.titleEn}  ({item.fields.year})</Typography>
+            <Typography gutterBottom variant="subtitle2" color="textSecondary">{item.fields.titleBr}</Typography>
+            {item.fields.tags["tags"].map(tag=>(<Chip label={tag} />))}
+            <Typography variant="body2" gutterBottom paragraph>{item.fields.description}</Typography>
+            <Typography variant="body2" color="textSecondary">{item.fields.author}</Typography>
+            <CardActions>
+            <Link href={item.fields.url} variant="body2">Trailer</Link>
+
+            </CardActions>
+          </CardContent>
+        </Card>
       ))}
       </div>
-  )
+    )
 }
-
 
 const contentfulClient = new ContentfulClient(apiconfig);
 
